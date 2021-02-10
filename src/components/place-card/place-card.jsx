715b8ed28457e1818fe3d1b034ab7dda24b284bd
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-02-10 18:37:52 +0300

    Обновлен компонент карточка предложения, добавлена валидация props

--- src/components/place-card/place-card.jsx
+++ src/components/place-card/place-card.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PlaceCard = () => {
-  return (
-    <article className="cities__place-card place-card">
+const PlaceCard = (props) => {
+  const {offer} = props;
+
+  const isPremium = () => {
+    if (offer.isPremium) {
       <div className="place-card__mark">
         <span>Premium</span>
-      </div>
+      </div>;
+    }
+  };
+
+  const setRating = () => {
+    const STAR_WIDTH = 20
+    return Math.round(offer.rating) * STAR_WIDTH + `%`;
+  };
+
+  const isFavorite = () => {
+    let buttonFavoriteClasses = [`place-card__bookmark-button`, `button`];
+    if (offer.isFavorite) {
+      buttonFavoriteClasses.push(`place-card__bookmark-button--active`);
+    }
+    return buttonFavoriteClasses.join(` `);
+  };
+
+  return (
+    <article className="cities__place-card place-card">
+      {isPremium()}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
+          <img className="place-card__image" src={offer.previewImage} width="260" height="200" alt="Place image"/>
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro;{offer.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className={isFavorite()} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
@@ -26,17 +48,30 @@ const PlaceCard = () => {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: 80 + `%`}}></span>
+            <span style={{width: setRating()}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#">{offer.title}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{offer.type}</p>
       </div>
     </article>
   );
+};
+
+PlaceCard.propTypes = {
+  offer: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    isFavorite: PropTypes.bool.isRequired,
+    isPremium: PropTypes.bool.isRequired,
+    previewImage: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired
+  })
 };
 
 export default PlaceCard;
