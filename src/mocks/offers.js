36403e4f2fc8f0d17fca7ceb8dade3a32ff0a163
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-02-10 16:39:27 +0300

    Добавлены моковые данные

---
+++ src/mocks/offers.js
@@ -0,0 +1,42 @@
+export const OFFERS = [
+  {
+    "id": 1,
+    "is_favorite": false,
+    "is_premium": false,
+    "preview_image": "./img/apartment-01.jpg",
+    "price": 120,
+    "rating": 4.8,
+    "title": "Beautiful & luxurious studio at great location",
+    "type": "room"
+  },
+  {
+    "id": 2,
+    "is_favorite": true,
+    "is_premium": false,
+    "preview_image": "./img/apartment-02.jpg",
+    "price": 130,
+    "rating": 4.5,
+    "title": "Nice, cozy, warm big bed apartment",
+    "type": "apartment"
+  },
+  {
+    "id": 3,
+    "is_favorite": false,
+    "is_premium": true,
+    "preview_image": "./img/apartment-03.jpg",
+    "price": 140,
+    "rating": 4.7,
+    "title": "Canal View Prinsengracht",
+    "type": "house"
+  },
+  {
+    "id": 4,
+    "is_favorite": true,
+    "is_premium": true,
+    "preview_image": "./img/room.jpg",
+    "price": 150,
+    "rating": 4.3,
+    "title": "Wood and stone place",
+    "type": "hotel"
+  },
+];
