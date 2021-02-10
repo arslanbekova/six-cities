823401fced28990a63dbd9ef95c4271fd85c698c
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-02-10 18:38:12 +0300

    Исправлены ошибки eslint

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -1,42 +1,42 @@
 export const OFFERS = [
   {
     "id": 1,
-    "is_favorite": false,
-    "is_premium": false,
-    "preview_image": "./img/apartment-01.jpg",
+    "isFavorite": false,
+    "isPremium": false,
+    "previewImage": `./img/apartment-01.jpg`,
     "price": 120,
     "rating": 4.8,
-    "title": "Beautiful & luxurious studio at great location",
-    "type": "room"
+    "title": `Beautiful & luxurious studio at great location`,
+    "type": `room`
   },
   {
     "id": 2,
-    "is_favorite": true,
-    "is_premium": false,
-    "preview_image": "./img/apartment-02.jpg",
+    "isFavorite": true,
+    "isPremium": false,
+    "previewImage": `./img/apartment-02.jpg`,
     "price": 130,
-    "rating": 4.5,
-    "title": "Nice, cozy, warm big bed apartment",
-    "type": "apartment"
+    "rating": 4.3,
+    "title": `Nice, cozy, warm big bed apartment`,
+    "type": `apartment`
   },
   {
     "id": 3,
-    "is_favorite": false,
-    "is_premium": true,
-    "preview_image": "./img/apartment-03.jpg",
+    "isFavorite": false,
+    "isPremium": true,
+    "previewImage": `./img/apartment-03.jpg`,
     "price": 140,
-    "rating": 4.7,
-    "title": "Canal View Prinsengracht",
-    "type": "house"
+    "rating": 4.0,
+    "title": `Canal View Prinsengracht`,
+    "type": `house`
   },
   {
     "id": 4,
-    "is_favorite": true,
-    "is_premium": true,
-    "preview_image": "./img/room.jpg",
+    "isFavorite": true,
+    "isPremium": true,
+    "previewImage": `./img/room.jpg`,
     "price": 150,
-    "rating": 4.3,
-    "title": "Wood and stone place",
-    "type": "hotel"
+    "rating": 3.2,
+    "title": `Wood and stone place`,
+    "type": `hotel`
   },
 ];
