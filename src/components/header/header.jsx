c57533ebbea00158816a1d1d2da63420ccb60530
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-02-16 23:47:33 +0300

    Добавлена валидация для props children

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const Header = (props) => (
   <header className="header">
@@ -23,8 +24,12 @@ const Header = (props) => (
         </nav>
       </div>
     </div>
-  {props.children}
+    {props.children}
   </header>
 );
+
+Header.propTypes = {
+  children: PropTypes.element
+};
 
 export default Header;
