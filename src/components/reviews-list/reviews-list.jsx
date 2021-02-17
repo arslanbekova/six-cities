3542d3680ec537dd90cd662ef12a0a307504ccc4
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-02-17 14:11:20 +0300

    Добавлены компоненты Review, ReviewsList; Созданы моки для отзывов

---
+++ src/components/reviews-list/reviews-list.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReviewItem from '../review-item/review-item';
+
+const ReviewItem = () => {
+  return (
+    <ul className="reviews__list"></ul>
+  );
+};
+
+export default ReviewItem;
