09ebde5b10c496d732697826d77c4cf787d134a3
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-03-02 18:20:30 +0300

    Удалена передача моков из props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import PropTypes from 'prop-types';
 import Main from '../main/main';
 import SignIn from '../sign-in/sign-in';
 import Favorites from '../favorites/favorites';
 import Room from '../room/room';
 import NotFound from '../not-found/not-found';
 
-const App = (props) => {
-  const {offers, reviews} = props;
+const App = () => {
   return (
     <BrowserRouter>
       <Switch>
@@ -19,10 +17,10 @@ const App = (props) => {
           <SignIn/>
         </Route>
         <Route exact path="/favorites">
-          <Favorites offers={offers}/>
+          <Favorites/>
         </Route>
         <Route exact path="/offer/:id">
-          <Room reviews={reviews} offers={offers}/>
+          <Room/>
         </Route>
         <Route>
           <NotFound/>
@@ -30,31 +28,6 @@ const App = (props) => {
       </Switch>
     </BrowserRouter>
   );
-};
-
-App.propTypes = {
-  offers: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    isFavorite: PropTypes.bool.isRequired,
-    isPremium: PropTypes.bool.isRequired,
-    previewImage: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    rating: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired
-  })),
-  reviews: PropTypes.arrayOf(PropTypes.shape({
-    comment: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    id: PropTypes.number.isRequired,
-    rating: PropTypes.number.isRequired,
-    user: PropTypes.shape({
-      avatarUrl: PropTypes.string.isRequired,
-      id: PropTypes.number.isRequired,
-      isPro: PropTypes.bool.isRequired,
-      name: PropTypes.string.isRequired,
-    })
-  }))
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import App from './components/app/app';
 import {createStore} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {Provider} from 'react-redux';
-import {REVIEWS} from './mocks/reviews';
 import {reducer} from './store/reducer';
 
 const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
     <Provider store={store}>
-      <App
-        reviews={REVIEWS}
-      />
+      <App/>
     </Provider>,
     document.querySelector(`#root`)
 );
