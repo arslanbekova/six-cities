a56ab30ebdcba5d0c5cb163d1f7bd9d44dc76870
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-03-10 01:10:48 +0300

    Добавлен адаптер для данных с сервера

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,6 +1,13 @@
 import {ActionCreator} from "./action";
+import camelcaseKeys from 'camelcase-keys'
 
 export const fetchOffersList = () => (dispatch, _getState, api) => (
-  api.get(`/hotels`)
+  api.get(`/hotels`, {
+      transformResponse: [
+        (data) => {
+          return camelcaseKeys(JSON.parse(data), { deep: true })
+        }
+      ]
+    })
     .then(({data}) => dispatch(ActionCreator.loadOffers(data)))
 );
