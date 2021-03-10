cce28782d3dff0345c8d95f36e4f55fd3e9ec601
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-03-10 16:43:09 +0300

    Добавлена проверка прохождения авторизации

--- src/index.js
+++ src/index.js
@@ -7,12 +7,19 @@ import {createAPI} from "./services/api";
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {Provider} from 'react-redux';
 import {reducer} from './store/reducer';
+import {ActionCreator} from './store/action';
+import {checkAuth} from "./store/api-actions";
+import {AuthorizationStatus} from "./utils/const";
 
-const api = createAPI();
+const api = createAPI(
+    () => store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH))
+);
 
 const store = createStore(reducer, composeWithDevTools(
     applyMiddleware(thunk.withExtraArgument(api))
 ));
+
+store.dispatch(checkAuth());
 
 ReactDOM.render(
     <Provider store={store}>
