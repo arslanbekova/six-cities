39b653235c9ea686c651db598bb941ea048609a0
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-03-19 16:21:12 +0300

    Добавлен условный рендеринг страницы favorites

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -1,42 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
-import FavoritePlaceCard from '../favorite-place-card/favorite-place-card';
 import Header from '../header/header';
+import FavoritesEmpty from '../favorites-empty/favorites-empty';
+import FavoritesList from '../favorites-list/favorites-list';
 import {connect} from 'react-redux';
 import {offerTypes} from '../../prop-types/prop-types';
+import {fetchFavoritesOffers} from "../../store/api-actions";
+import Spinner from '../spinner/spinner';
 
 const Favorites = (props) => {
-  const {offers} = props;
+  const {favoritesOffers, isFavoritesOffersLoaded, onOpenFavoritesPage} = props;
 
-  const favoritesOffers = offers.filter((offer) => offer.isFavorite === true).reduce((result, item) => {
-    result[item.city.name] = [...result[item.city.name] || [], item];
-    return result;
-  }, {});
+  useEffect(() => {
+    if (!isFavoritesOffersLoaded) {
+      onOpenFavoritesPage();
+    }
+  }, [isFavoritesOffersLoaded]);
+
+  if (!isFavoritesOffersLoaded) {
+    return (
+      <Spinner/>
+    );
+  }
 
   return (
     <div className="page">
       <Header/>
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              {Object.keys(favoritesOffers).map((cityKey) => <li className="favorites__locations-items" key={cityKey}>
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <a className="locations__item-link" href="#">
-                      <span>{cityKey}</span>
-                    </a>
-                  </div>
-                </div>
-                <div className="favorites__places">
-                  {Object.values(favoritesOffers).map((city) => city.map((offer) => offer.city.name === cityKey ? <FavoritePlaceCard key={offer.id} offer={offer}/> : ``))}
-                </div>
-              </li>)}
-            </ul>
-          </section>
-        </div>
-      </main>
+      {favoritesOffers.length === 0 ? <FavoritesEmpty/> : <FavoritesList/>}
       <footer className="footer container">
         <a className="footer__logo-link" href="main.html">
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
@@ -47,14 +37,23 @@ const Favorites = (props) => {
 };
 
 Favorites.propTypes = {
-  offers: PropTypes.arrayOf(PropTypes.shape(offerTypes)).isRequired
+  favoritesOffers: PropTypes.arrayOf(PropTypes.shape(offerTypes)).isRequired,
+  onOpenFavoritesPage: PropTypes.func.isRequired,
+  isFavoritesOffersLoaded: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = (state) => {
   return {
-    offers: state.offers,
+    favoritesOffers: state.favoritesOffers,
+    isFavoritesOffersLoaded: state.isFavoritesOffersLoaded
   };
 };
 
+const mapDispatchToProps = (dispatch) => ({
+  onOpenFavoritesPage() {
+    dispatch(fetchFavoritesOffers());
+  },
+});
+
 export {Favorites};
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
