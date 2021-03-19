8b0c5fec04902b4e7581f0078f960a315b200c4c
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-03-19 16:19:30 +0300

    Список избранных предложений вынесен в отдельный компонент

---
+++ src/components/favorites-list/favorites-list.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {offerTypes} from '../../prop-types/prop-types';
+import FavoritePlaceCard from '../favorite-place-card/favorite-place-card';
+
+const FavoritesList = (props) => {
+  const {favoritesOffers} = props;
+
+  favoritesOffers.reduce((result, item) => {
+    result[item.city.name] = [...result[item.city.name] || [], item];
+    return result;
+  }, {});
+
+  return (
+    <main className="page__main page__main--favorites">
+      <div className="page__favorites-container container">
+        <section className="favorites">
+          <h1 className="favorites__title">Saved listing</h1>
+          <ul className="favorites__list">
+            {Object.keys(favoritesOffers).map((cityKey) => <li className="favorites__locations-items" key={cityKey}>
+              <div className="favorites__locations locations locations--current">
+                <div className="locations__item">
+                  <a className="locations__item-link" href="#">
+                    <span>{cityKey}</span>
+                  </a>
+                </div>
+              </div>
+              <div className="favorites__places">
+                {Object.values(favoritesOffers).map((city) => city.map((offer) => offer.city.name === cityKey ? <FavoritePlaceCard key={offer.id} offer={offer}/> : ``))}
+              </div>
+            </li>)}
+          </ul>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+FavoritesList.propTypes = {
+  favoritesOffers: PropTypes.arrayOf(PropTypes.shape(offerTypes)).isRequired
+};
+
+export default FavoritesList;
