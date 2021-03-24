cb7ae6373a5f15e6e03303a8be30d743651581d1
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-03-24 13:55:59 +0300

    Добавлена кнопка logout

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,15 +3,26 @@ import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {authTypes} from '../../prop-types/prop-types';
-import {fetchFavoritesOffers} from "../../store/api-actions";
+import {fetchFavoritesOffers, logout} from "../../store/api-actions";
 
 const Header = (props) => {
-  const {authorizationStatus, authInfo, onOpenFavoritesPage} = props;
+  const {authorizationStatus, authInfo, onOpenFavoritesPage, onClickLogoutButton} = props;
 
   const handleOpenFavoritesPage = () => {
     if (authorizationStatus) {
       onOpenFavoritesPage();
     }
+  };
+
+  const logoutButtonStyle = {
+    marginLeft: 10 + `px`,
+    width: 18 + `px`,
+    height: 18 + `px`,
+    backgroundImage: `url(../img/logout.svg)`
+  };
+
+  const handleLogout = () => {
+    onClickLogoutButton();
   };
 
   return (
@@ -25,12 +36,13 @@ const Header = (props) => {
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
-              <li className="header__nav-item user">
+              <li className="header__nav-item user" style={{display: `flex`, alignItems: `baseline`}}>
                 <Link className="header__nav-link header__nav-link--profile" to="/favorites" onClick={handleOpenFavoritesPage}>
                   <div className="header__avatar-wrapper user__avatar-wrapper" style={authorizationStatus ? {backgroundImage: `url(${authInfo.avatarUrl})`} : undefined}>
                   </div>
                   <span className={authorizationStatus && `header__user-name user__name` || `header__login`}>{authorizationStatus && authInfo.email || `Sign in`}</span>
                 </Link>
+                {authorizationStatus && <button className="header__logout-button button" type="button" onClick={handleLogout} style={logoutButtonStyle}></button>}
               </li>
             </ul>
           </nav>
@@ -43,7 +55,8 @@ const Header = (props) => {
 Header.propTypes = {
   authorizationStatus: PropTypes.bool.isRequired,
   authInfo: PropTypes.shape(authTypes),
-  onOpenFavoritesPage: PropTypes.func.isRequired
+  onOpenFavoritesPage: PropTypes.func.isRequired,
+  onClickLogoutButton: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
@@ -55,6 +68,9 @@ const mapDispatchToProps = (dispatch) => ({
   onOpenFavoritesPage() {
     dispatch(fetchFavoritesOffers());
   },
+  onClickLogoutButton() {
+    dispatch(logout());
+  }
 });
 
 export {Header};
